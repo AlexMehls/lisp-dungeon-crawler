a5b9d6523d247b16ee6df1bd3c21362f88fc2cd4
Alexander Mehls <alexmehls@t-online.de>
2023-11-04 17:28:59 +0100

    input and fps counter

--- openGlTest.lisp
+++ openGlTest.lisp
@@ -7,6 +7,9 @@
 (ql:quickload :cl-cffi-gtk)
 (ql:quickload :3d-vectors)
 (ql:quickload :3d-matrices)
+(ql:quickload :local-time)
+(ql:quickload :queues)
+(asdf:oos 'asdf:load-op :queues.simple-queue)
 
 (defpackage :opengl-test
 (:use :gtk :gdk :gdk-pixbuf :gobject
@@ -94,10 +97,25 @@
 
 (defvar *default-camera* (make-instance 'camera :position (3d-vectors:vec 0 0) :screen-size 7))
 
-(defun example-gl-area ()
+(defvar *keys-held* NIL)
+(defvar *keys-pressed* NIL)
+
+(defmacro keyval (name)
+  (gdk-keyval-from-name name)) ; Only needs to be evaluated once -> macro
+
+(defmacro get-key-press (name)
+  `(member (keyval ,name) *keys-pressed*))
+
+(defmacro get-key-hold (name)
+  `(member (keyval ,name) *keys-held*))
+
+(defun main ()
   (within-main-loop
     (let ((window (gtk-window-new :toplevel))
-          (area (make-instance 'gtk-gl-area))
+          (overlay (gtk-overlay-new))
+          (area (make-instance 'gtk-gl-area :auto-render T)) ; maybe render manually?
+          (fixed-container (gtk-fixed-new))
+          (fps-counter (gtk-label-new "FPS:"))
           vao
           (test-sprite (make-instance 'sprite
                          :position (3d-vectors:vec 0 0)
@@ -106,8 +124,16 @@
                          :texture *test-texture2*))
           (test-tiles (make-instance 'tile-array
                         :tiles (make-array '(7 5) :initial-element (make-instance 'tile))
-                        :offset (3d-vectors:vec -3 -2))))
-      (gtk-container-add window area)
+                        :offset (3d-vectors:vec -3 -2)))
+          (prev-time (local-time:now))
+          (curr-time (local-time:now))
+          (fps-vals (queues:make-queue :simple-queue)))
+      (gtk-container-add window overlay)
+      (gtk-container-add overlay area)
+      (gtk-overlay-add-overlay overlay fixed-container)
+      (gtk-fixed-put fixed-container fps-counter 0 0)
+      ;(gtk-overlay-add-overlay overlay fps-counter)
+      (gtk-widget-add-events window :key-press-mask)
       (g-signal-connect area "realize"
                         (lambda (widget)
                           (declare (ignore widget))
@@ -130,29 +156,79 @@
       (g-signal-connect area "render"
                         (lambda (area context)
                           (declare (ignore context))
+                          (setf curr-time (local-time:now))
+                          ;(format t "Time since last render: ~ams~%" (* 1000 (local-time:timestamp-difference curr-time prev-time)))
+                          ;(format t "FPS: ~a~%" (/ 1 (local-time:timestamp-difference curr-time prev-time)))
+
+                          (let* ((delta-time (local-time:timestamp-difference curr-time prev-time))
+                                 (move-dist (* 2 delta-time)))
+                            (queues:qpush fps-vals (/ 1 delta-time))
+                            (when (> (queues:qsize fps-vals) 60)
+                                  (queues:qpop fps-vals))
+                            (let ((avg-fps 0))
+                              (queues:map-queue
+                                (lambda (element)
+                                  (setf avg-fps (+ avg-fps element)))
+                                fps-vals)
+                              (setf avg-fps (/ avg-fps (queues:qsize fps-vals)))
+                              (gtk-label-set-text fps-counter (concatenate 'string "FPS: " (write-to-string (round avg-fps)))))
+                          
+                            ;(when *keys-pressed*
+                            ;      (format t "Pressed: ~a~%" *keys-pressed*))
+                            (when (get-key-hold "w")
+                                  (setf (sprite-position test-sprite) (3d-vectors:v+ (sprite-position test-sprite) (3d-vectors:vec 0 move-dist))))
+                            (when (get-key-hold "a")
+                                  (setf (sprite-position test-sprite) (3d-vectors:v+ (sprite-position test-sprite) (3d-vectors:vec (- move-dist) 0))))
+                            (when (get-key-hold "s")
+                                  (setf (sprite-position test-sprite) (3d-vectors:v+ (sprite-position test-sprite) (3d-vectors:vec 0 (- move-dist)))))
+                            (when (get-key-hold "d")
+                                  (setf (sprite-position test-sprite) (3d-vectors:v+ (sprite-position test-sprite) (3d-vectors:vec move-dist 0)))))
 
                           (gl:clear-color 0.5 0.5 0.5 1.0)
                           (gl:clear :color-buffer :depth-buffer)
-
-                          (let ((w (gtk-widget-get-allocated-width area))
-                                (h (gtk-widget-get-allocated-height area)))
-                            (when (and (> h 0) (> w 0))
-                                (setf (slot-value *default-camera* 'screen-ratio) (/ w h))))
                           
                           (let ((vp-mat (camera-view-projection-matrix *default-camera*)))
                             (tile-array-draw test-tiles vp-mat)
                             (sprite-draw test-sprite vp-mat))
                           
+                          (setf *keys-pressed* NIL)
+                          (setf prev-time curr-time)
+                          (gtk-gl-area-queue-render area) ; maybe render manually?
+                          NIL))
+      (g-signal-connect area "resize"
+                        (lambda (area width height)
+                          (declare (ignore area))
+                          (when (and (> height 0) (> width 0))
+                              (setf (slot-value *default-camera* 'screen-ratio) (/ width height)))
                           NIL))
       (g-signal-connect window "destroy"
                         (lambda (widget)
                           (declare (ignore widget))
                           (leave-gtk-main)))
+      (g-signal-connect window "key_press_event"
+                        (lambda (widget event)
+                          (declare (ignore widget))
+                          (let ((keyval (gdk-event-key-keyval event)))
+                            (unless (member keyval *keys-held*)
+                              (setf *keys-pressed* (adjoin keyval *keys-pressed*)))
+                            (setf *keys-held* (adjoin keyval *keys-held*)))
+                          ;(format t "Event: ~a~%" event)
+                          ;(format t "Keys: ~a~%" *keys-held*)
+                          ))
+      (g-signal-connect window "key_release_event"
+                        (lambda (widget event)
+                          (declare (ignore widget))
+                          (setf *keys-held* (set-difference *keys-held* `(,(gdk-event-key-keyval event))))
+                          ;(format t "Event: ~a~%" event)
+                          ;(when (= (gdk-event-key-keyval event))
+                          ;    (format t "pressed space~%"))
+                          ;(format t "Keys: ~a~%" *keys-held*)
+                          ))
       (gtk-widget-show-all window)))
   (sb-thread:release-foreground) ; For better debug output
   (gtk:join-gtk-main))
 
-(example-gl-area)
+(main)
 ;(sb-ext:save-lisp-and-die "openGlTest.exe"
-;                   :toplevel #'example-gl-area
+;                   :toplevel #'main
 ;                   :executable t)
